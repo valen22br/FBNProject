6c9a23fa9c99cc584e60b1c937e00108763ca8e4
Luis Gustavo Grubert Valensuela <valensuela1@gmail.com>
2017-07-10 14:31:01 -0400

    Create frontend.php

---
+++ frontend.php
@@ -0,0 +1,234 @@
+Last login: Mon Jul 10 12:26:15 on ttys002
+You have new mail.
+ValenMaBookPro2:Debug valen$ 
+ValenMaBookPro2:Debug valen$ 
+ValenMaBookPro2:Debug valen$ 
+ValenMaBookPro2:Debug valen$ 
+ValenMaBookPro2:Debug valen$ ssh fbnc4069775985@fbnconstrucitongc.com
+ssh_exchange_identification: Connection closed by remote host
+ValenMaBookPro2:Debug valen$ ssh fbnc4069775985@fbnconstructiongc.com
+Last login: Sun May 21 08:48:47 2017 from 99.33.199.208
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-bash-4.2$ pwd
+/home/fbnc4069775985
+-bash-4.2$ ls -l
+total 70
+drwx---r-x 2 fbnc4069775985 inetuser 139 Jun  9 21:14 data
+drwx---r-x 5 fbnc4069775985 inetuser 670 Jul  9 18:10 html
+drwx---r-x 2 fbnc4069775985 inetuser   0 Jun 24 07:54 tmp
+-bash-4.2$ cd html/
+-bash-4.2$ ls
+gd-config.php  wp-activate.php       wp-content         wp-login.php      xmlrpc.php
+index.php      wp-admin              wp-cron.php        wp-mail.php
+license.txt    wp-blog-header.php    wp-includes        wp-settings.php
+readme.html    wp-comments-post.php  wp-links-opml.php  wp-signup.php
+robots.txt     wp-config.php         wp-load.php        wp-trackback.php
+-bash-4.2$ ls -l
+total 961
+-r--r--r--  1 root                  0  1026 May 11 12:07 gd-config.php
+-r--r--r--  1 root                  0   418 Sep 24  2013 index.php
+-r--r--r--  1 root                  0 19935 Jan  2  2017 license.txt
+-r--r--r--  1 root                  0  7413 Jun  9 21:14 readme.html
+-rw----r--  1 fbnc4069775985 inetuser    24 May 25 10:18 robots.txt
+-r--r--r--  1 root                  0  5447 Sep 27  2016 wp-activate.php
+dr-xr-xr-x  9 root                  0  2690 May 25 10:20 wp-admin
+-r--r--r--  1 root                  0   364 Dec 19  2015 wp-blog-header.php
+-r--r--r--  1 root                  0  1627 Aug 29  2016 wp-comments-post.php
+-rw-------  1 fbnc4069775985 inetuser  2963 May 11 12:07 wp-config.php
+drwx---r-x  8 fbnc4069775985 inetuser   215 May 25 10:20 wp-content
+-r--r--r--  1 root                  0  3286 May 24  2015 wp-cron.php
+dr-xr-xr-x 18 root                  0  6624 May 25 10:19 wp-includes
+-r--r--r--  1 root                  0  2422 Nov 20  2016 wp-links-opml.php
+-r--r--r--  1 root                  0  3301 Oct 24  2016 wp-load.php
+-r--r--r--  1 root                  0 34327 Jun  9 21:14 wp-login.php
+-r--r--r--  1 root                  0  8048 Jan 10 22:15 wp-mail.php
+-r--r--r--  1 root                  0 16200 Jun  9 21:14 wp-settings.php
+-r--r--r--  1 root                  0 29924 Jun  9 21:14 wp-signup.php
+-r--r--r--  1 root                  0  4513 Oct 14  2016 wp-trackback.php
+-r--r--r--  1 root                  0  3065 Aug 31  2016 xmlrpc.php
+-bash-4.2$ find . -type f | xargs grep -l "regency-author"
+./wp-content/themes/constructent/style.css
+grep: ./wp-content/upgrade/themeforest-12910459-constructent-responsive-construction-wordpress-theme/Constructent: No such file or directory
+grep: Packageconstructent-child.zip: No such file or directory
+grep: ./wp-content/upgrade/themeforest-12910459-constructent-responsive-construction-wordpress-theme/Constructent: No such file or directory
+grep: Packagedocumentation.html: No such file or directory
+grep: ./wp-content/upgrade/themeforest-12910459-constructent-responsive-construction-wordpress-theme/Constructent: No such file or directory
+grep: Packageconstructent.zip: No such file or directory
+grep: ./wp-content/upgrade/themeforest-12910459-constructent-responsive-construction-wordpress-theme/Constructent: No such file or directory
+grep: Packagedemo-content.xml: No such file or directory
+grep: ./wp-content/upgrade/themeforest-12910459-constructent-responsive-construction-wordpress-theme/Constructent: No such file or directory
+grep: PackagePluginshome_slider.zip: No such file or directory
+grep: ./wp-content/upgrade/themeforest-12910459-constructent-responsive-construction-wordpress-theme/Constructent: No such file or directory
+grep: Packagetheme-options.txt: No such file or directory
+grep: ./wp-content/upgrade/themeforest-12910459-constructent-responsive-construction-wordpress-theme/Constructent: No such file or directory
+grep: PackagePluginsconstructent-demo-import.zip: No such file or directory
+./wp-content/plugins/constructent-shortcodes/inc/frontend.php
+./wp-content/plugins/constructent-shortcodes/css/frontend.css
+./wp-content/plugins/constructent-shortcodes/css/frontend.less
+-bash-4.2$ packet_write_wait: Connection to 198.71.233.227: Broken pipe
+ValenMaBookPro2:Debug valen$ ./na
+-bash: ./na: No such file or directory
+ValenMaBookPro2:Debug valen$ ssh fbnc4069775985@fbnconstructiongc.com
+Creating directory '/home/fbnc4069775985'.
+
+-bash-4.2$ cd html/
+-bash-4.2$ ls -lhctr
+total 961K
+dr-xr-xr-x 18 root                  0 6.5K May 25 10:20 wp-includes
+-rw----r--  1 fbnc4069775985 inetuser   24 May 25 10:20 robots.txt
+-r--r--r--  1 root                  0 3.3K May 25 10:20 wp-load.php
+-r--r--r--  1 root                  0 1.6K May 25 10:20 wp-comments-post.php
+-r--r--r--  1 root                  0 7.9K May 25 10:20 wp-mail.php
+-r--r--r--  1 root                  0 4.5K May 25 10:20 wp-trackback.php
+-r--r--r--  1 root                  0 3.3K May 25 10:20 wp-cron.php
+-r--r--r--  1 root                  0  364 May 25 10:20 wp-blog-header.php
+-r--r--r--  1 root                  0 2.4K May 25 10:20 wp-links-opml.php
+-r--r--r--  1 root                  0 5.4K May 25 10:20 wp-activate.php
+-r--r--r--  1 root                  0 3.0K May 25 10:20 xmlrpc.php
+-r--r--r--  1 root                  0  418 May 25 10:20 index.php
+-r--r--r--  1 root                  0  20K May 25 10:20 license.txt
+-r--r--r--  1 root                  0 1.1K May 25 10:20 gd-config.php
+-rw-------  1 fbnc4069775985 inetuser 2.9K May 25 10:20 wp-config.php
+dr-xr-xr-x  9 root                  0 2.7K May 25 10:20 wp-admin
+drwx---r-x  8 fbnc4069775985 inetuser  215 May 25 10:20 wp-content
+-r--r--r--  1 root                  0 7.3K Jun  9 21:14 readme.html
+-r--r--r--  1 root                  0  34K Jun  9 21:14 wp-login.php
+-r--r--r--  1 root                  0  16K Jun  9 21:14 wp-settings.php
+-r--r--r--  1 root                  0  30K Jun  9 21:14 wp-signup.php
+-bash-4.2$ find . -type f | grep -rin "regency-author"
+wp-content/themes/constructent/style.css:3464:.red .fitsc-testimonials .regency-author {
+wp-content/themes/constructent/style.css:4308:.orange .fitsc-testimonials .regency-author {
+wp-content/themes/constructent/style.css:5152:.green .fitsc-testimonials .regency-author {
+wp-content/themes/constructent/style.css:5996:.blue .fitsc-testimonials .regency-author {
+wp-content/plugins/constructent-shortcodes/inc/frontend.php:607:				<span class="regency-author">( %s )</span>
+wp-content/plugins/constructent-shortcodes/css/frontend.css:1408:.fitsc-testimonials .regency-author {
+wp-content/plugins/constructent-shortcodes/css/frontend.less:1103:	.regency-author {
+-bash-4.2$ vim wp-con
+wp-config.php  wp-content/    
+-bash-4.2$ vim wp-con
+wp-config.php  wp-content/    
+-bash-4.2$ vim wp-content/plugins/constructent-shortcodes/inc/frontend.php 
+-bash-4.2$ 
+-bash-4.2$ 
+-bash-4.2$ 
+-bash-4.2$ find . -type f | xargs grep -rin "get_the_content"
+./frontend.php:616:				get_the_content()
+-bash-4.2$ cd ../../../../
+-bash-4.2$ pwd
+/home/fbnc4069775985/html
+-bash-4.2$ find . -type f | xargs grep -rin "get_the_content"
+./wp-content/themes/twentyseventeen/template-parts/post/content-audio.php:45:		$content = apply_filters( 'the_content', get_the_content() );
+./wp-content/themes/twentyseventeen/template-parts/post/content-video.php:45:		$content = apply_filters( 'the_content', get_the_content() );
+./wp-content/themes/twentyfifteen/inc/template-tags.php:220:	$has_url = get_url_in_content( get_the_content() );
+./wp-content/themes/constructent/lib/frontend/entry.php:16:	$content = wp_strip_all_tags( strip_shortcodes( get_the_content() ) );
+grep: ./wp-content/upgrade/themeforest-12910459-constructent-responsive-construction-wordpress-theme/Constructent: No such file or directory
+grep: Packageconstructent-child.zip: No such file or directory
+grep: ./wp-content/upgrade/themeforest-12910459-constructent-responsive-construction-wordpress-theme/Constructent: No such file or directory
+grep: Packagedocumentation.html: No such file or directory
+grep: ./wp-content/upgrade/themeforest-12910459-constructent-responsive-construction-wordpress-theme/Constructent: No such file or directory
+grep: Packageconstructent.zip: No such file or directory
+grep: ./wp-content/upgrade/themeforest-12910459-constructent-responsive-construction-wordpress-theme/Constructent: No such file or directory
+grep: Packagedemo-content.xml: No such file or directory
+grep: ./wp-content/upgrade/themeforest-12910459-constructent-responsive-construction-wordpress-theme/Constructent: No such file or directory
+grep: PackagePluginshome_slider.zip: No such file or directory
+grep: ./wp-content/upgrade/themeforest-12910459-constructent-responsive-construction-wordpress-theme/Constructent: No such file or directory
+grep: Packagetheme-options.txt: No such file or directory
+grep: ./wp-content/upgrade/themeforest-12910459-constructent-responsive-construction-wordpress-theme/Constructent: No such file or directory
+grep: PackagePluginsconstructent-demo-import.zip: No such file or directory
+./wp-content/plugins/constructent-shortcodes/inc/frontend.php:616:				get_the_content()
+./wp-content/plugins/revslider/public/views/revslider-page-template.php:30:			echo do_shortcode( get_the_content() );
+./wp-includes/feed-rss2.php:101:	<?php $content = get_the_content_feed('rss2'); ?>
+./wp-includes/feed.php:180: * @see get_the_content()
+./wp-includes/feed.php:185:function get_the_content_feed($feed_type = null) {
+./wp-includes/feed.php:190:	$content = apply_filters( 'the_content', get_the_content() );
+./wp-includes/feed.php:212:	echo get_the_content_feed($feed_type);
+./wp-includes/deprecated.php:1654:	$content = get_the_content($more_link_text, $stripteaser);
+./wp-includes/formatting.php:3292:		$text = get_the_content('');
+./wp-includes/post-template.php:231:	$content = get_the_content( $more_link_text, $strip_teaser );
+./wp-includes/post-template.php:260:function get_the_content( $more_link_text = null, $strip_teaser = false ) {
+-bash-4.2$ vim wp-includes/post-t
+post-template.php            post-thumbnail-template.php  
+-bash-4.2$ vim wp-includes/post-t
+post-template.php            post-thumbnail-template.php  
+-bash-4.2$ vim wp-includes/post-template.php +231
+-bash-4.2$ 
+-bash-4.2$ 
+-bash-4.2$ 
+-bash-4.2$ pwd
+/home/fbnc4069775985/html
+-bash-4.2$ cd wp-content/plugins/constructent-
+-bash: cd: wp-content/plugins/constructent-: No such file or directory
+-bash-4.2$ cd wp-content/plugins/constructent-
+constructent-demo-import/ constructent-shortcodes/  
+-bash-4.2$ cd wp-content/plugins/constructent-shortcodes/
+-bash-4.2$ ls
+constructent-shortcodes.php  css  img  inc  js  languages
+-bash-4.2$ cd inc/
+-bash-4.2$ ls
+admin.php  frontend.php  modals  tpl
+-bash-4.2$ vim frontend.php +676
+-bash-4.2$ vim frontend.php +676
+
+                );
+
+                if ( $atts['category'] )
+                {
+                        $args['tax_query'] = array(
+                                array(
+                                        'taxonomy' => 'portfolio_category',
+                                        'field'    => 'slug',
+                                        'terms'    => $atts['category'],
+                                )
+                        );
+                }
+
+                if ( 'completion' == $atts['orderby'] )
+                {
+                        $args['orderby'] = 'meta_value_num';
+                        $args['meta_key'] = 'completion';
+                }
+
+                if ( 'completed' == $atts['type'] )
+                {
+                        $args['meta_query'] = array(
+                                array(
+                                        'key'     => 'completion',
+                                        'value'   => 100,
+                                        'compare' => '=',
+                                ),
+                        );
+                }
+                elseif ( 'under_construction' == $atts['type'] )
+                {
+                        $args['meta_query'] = array(
+                                array(
+                                        'key'     => 'completion',
+                                        'value'   => 100,
+                                        'compare' => '>',
+"frontend.php" [dos] 1344L, 33788C                                      676,3-17      50%
